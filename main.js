1a7b3e10c51daded1854a76868ee2d800d962771
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2021-11-27 01:27:59 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  window.loadFile("renderer/index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
