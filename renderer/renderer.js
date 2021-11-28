225d0eb5d4e036d80baa2a2f81fae33632f740fb
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2021-11-28 19:02:01 +0530

    Fix: send textArea value to main process

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -3,7 +3,7 @@ const textArea = document.getElementById("text-area");
 const btnSave = document.getElementById("btn-save");
 
 const sendTextData = () => {
-  window.ipcRendererAPI.send("text_data", "hello");
+  window.ipcRendererAPI.send("text_data", textArea.value);
 };
 
 btnSave.addEventListener("click", () => {
